320679c99a83341164f1696d200db978d62cf785
Durga <durgapragada204@gmail.com>
2026-03-16 23:59:22 +0530

    initial commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@
  */
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://saigoai.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
